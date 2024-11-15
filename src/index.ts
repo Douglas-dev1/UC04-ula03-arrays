1f714419a030104a1a06b5d06cc770b688c0701a
Douglas <douglas.2903@aluno.pr.senac.br>
2024-11-14 21:45:46 -0300

    :balloon: Testando estruturas e vetores

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,21 @@
-console.log("Olá mundo")
+import {questionFloat} from 'readline-sync'
 
+const notas: number[] = [];
+
+for(let a = 0; a<5; a++){
+  const nota1 = questionFloat("Informe a nota " + (a+1)+": ");
+  notas[a] = nota1;
+}
+
+/*const nota1 = questionFloat("Informe a nota 1: ");
+const nota2 = questionFloat("Informe a nota 2: ");
+const nota3 = questionFloat("Informe a nota 3: ");
+const nota4 = questionFloat("Informe a nota 4: ");
+const nota5 = questionFloat("Informe a nota 5: ");*/
+
+const media = (notas[1] + notas[2] + notas[3] + notas[4] + notas[5] ) / 5;
+
+if(media>65)
+  console.log("Aluno aprovado!")
+else
+  console.log("Aluno reprovado!")
